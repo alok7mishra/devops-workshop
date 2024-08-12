e21ed97a11d1e2cb8288df4957aa4c24d4aa668a
AXWAY\almishra <almishra@axway.com>
2024-08-12 14:32:10 +0530

    Updated Ansible_latest and terraform_code_latest

--- terraform_code_latest/V4-EC2-With_VPC_for_each.tf
+++ terraform_code_latest/V4-EC2-With_VPC_for_each.tf
@@ -31,6 +31,14 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
   to_port           = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_jenkins_port" {
+  security_group_id = aws_security_group.allow_ssh.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 8080
+  ip_protocol       = "tcp"
+  to_port           = 8080
+}
+
 
 resource "aws_vpc_security_group_egress_rule" "allow_ssh_ipv4" {
   security_group_id = aws_security_group.allow_ssh.id
